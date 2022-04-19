cafb3102af52bc37c3164c84ae65f9f06d3c24d3
Amir Shariat <amirshariat1987@gmail.com>
2022-04-19 17:26:00 -0600

    replies done

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -24,6 +24,23 @@ const commentController = {
       .catch((err) => res.json(err));
   },
 
+  // add reply
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     // The first method used here, .findOneAndDelete(), works a lot like .findOneAndUpdate(), as it deletes the document while also returning its data. We then take that data and use it to identify and remove it from the associated pizza using the Mongo $pull operation. Lastly, we return the updated pizza data, now without the _id of the comment in the comments array, and return it to the user.
@@ -45,6 +62,17 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch((err) => res.json(err));
+  },
+
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => res.json(err));
   },
 };
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
@@ -9,6 +11,8 @@ router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 // After deleting a particular comment, you need to know exactly which pizza that comment originated from.
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
