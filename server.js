4ba6bf6afd4f66da54d9bbf123d1e551527a46d5
Amir Shariat <amirshariat1987@gmail.com>
2022-04-20 15:25:32 -0600

    added comment

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.static("public"));
 app.use(require("./routes"));
 
 // Set up Mongoose to connect when we start the app. In the preceding code, mongoose.connect() tells Mongoose which database we want to connect to. If the environment variable MONGODB_URI exists, like on Heroku when we deploy later, it will use that. Otherwise, it will short-circuit to the local MongoDB server's database at mongodb://localhost:27017/pizza-hunt. The second argument in the example is a set of configuration options Mongoose asks for more information about. MongoDB will find and connect to the database if it exists or create the database if it doesn't.
+// Just like JawsDB with MySQL-based applications, we want to set up our Pizza Hunt application so it tries to connect to a production database first and falls back to the local database's connection when it's in development.
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
